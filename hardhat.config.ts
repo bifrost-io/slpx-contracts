ee93441e94743c385199adcc1681696e27127ff2
Akagi201 <akagi201@gmail.com>
2022-10-28 09:09:47 +0800

    feat: add moonbase network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv"
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
+  solidity: {
+    compilers: [{
+      version: "0.8.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        }
+      }
+    }]
+  },
+  networks: {
+    moonbaseAlpha:{
+      url: "https://rpc.api.moonbase.moonbeam.network",
+      chainId: 1287,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY]: [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY || undefined,
+    enabled: process.env.REPORT_GAS? true : false,
+  },
 };
 
 export default config;
