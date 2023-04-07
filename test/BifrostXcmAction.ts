529ef081710d64c91f5fc948eeb171343f9eca76
hqwangningbo <2536935847@qq.com>
2023-04-07 23:39:44 +0800

    Add hardhat test example

--- test/BifrostXcmAction.ts
+++ test/BifrostXcmAction.ts
@@ -1,13 +1,22 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import "@nomiclabs/hardhat-ethers"
 
 describe("BifrostXcmAction", function () {
-  it("Deployment", async function*() {
-    const BifrostXcmAction = await ethers.getContractFactory("BifrostXcmAction");
-    const bifrostXcmAction = await BifrostXcmAction.deploy();
-    await bifrostXcmAction.deployed();
+  it("Deployment", async function() {
+    // const caller = await ethers.getSigner()
+    const AstarXcmAction = await ethers.getContractFactory("AstarXcmAction");
+    const astarXcmAction = await AstarXcmAction.deploy();
 
-    console.log(`BifrostXcmAction contract deployed to ${bifrostXcmAction.address}, owner: ${bifrostXcmAction.owner}`);
-    expect(true);
+    const owner = await astarXcmAction.owner();
+    expect(owner).to.equal("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+  });
+
+  it("Getbalance", async function() {
+    // BNC address
+    const astar = await ethers.getContractAt("IERC20","0xfFffFffF00000000000000010000000000000007");
+
+    const balance = await astar.totalSupply();
+    expect(balance).to.equal("1000000000000000");
   });
 });
